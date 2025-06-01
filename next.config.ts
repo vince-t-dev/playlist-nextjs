6ff3dd62593d48f5b699403a91024bf8cadbd09e
Vince T <vince@backbone.digital>
2025-06-01 12:55:41 +0900

    init. commit

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,20 @@
+const isProd = process.env.NODE_ENV === "production";
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: "export",
 	trailingSlash: true,
+
+	// expresia static deployment
+	basePath: isProd ? "/__xpr__/pub_engine/playlist-nextjs/web" : "",
+	assetPrefix: isProd ? "/__xpr__/pub_engine/playlist-nextjs/web/" : "",
+
 	// disable image optimization for static export compatibility
 	images: {
-		unoptimized: true
+		unoptimized: true,
 	},
+
+	// env variables
 	env: {
 		DOMAIN_URL: process.env.DOMAIN_URL,
 		PUBLIC_API_BASE: process.env.PUBLIC_API_BASE,
