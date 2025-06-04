14451f527d3a4e7fa6e26e223815ea59471daac6
Vince T <vince@backbone.digital>
2025-06-04 10:52:09 +0900

    init. commit

--- next.config.ts
+++ next.config.ts
@@ -8,25 +8,24 @@ const nextConfig = {
 	// expresia static deployment
 	// TODO!
 	// basePath: isProd ? "/__xpr__/pub_engine/playlist-nextjs/web" : "",
-	assetPrefix: isProd ? "/__xpr__/pub_engine/playlist-nextjs/web/" : "",
-
+	// assetPrefix: isProd ? "/__xpr__/pub_engine/playlist-nextjs/web/" : "",
+	assetPrefix: "/__xpr__/pub_engine/playlist-nextjs/web/",
 	// disable image optimization for static export compatibility
 	images: {
 		unoptimized: true,
 	},
 
 	// expresia static deployment: resolving json data paths
-	async rewrites() {
-		// return isProd
-			// ? [
-		return [
-				{
-					source: "/__xpr__/pub_engine/playlist-nextjs/web/_next/data/:path*",
-					destination: "/_next/data/:path*",
-				},
-			]
-			// : [];
-	},
+	// async rewrites() {
+	// 	return isProd
+	// 		? [
+	// 			{
+	// 				source: "/_next/data/:path*",
+	// 				destination: "/__xpr__/pub_engine/playlist-nextjs/web/_next/data/:path*"
+	// 			},
+	// 		]
+	// 		: [];
+	// },
 	
 	// env variables
 	env: {
