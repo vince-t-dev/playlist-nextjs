f44d1c9241cbdaedc1400e0f2d6f08cdf3aaf4c3
Vince T <vince@backbone.digital>
2025-06-04 11:01:07 +0900

    init. commit

--- next.config.ts
+++ next.config.ts
@@ -4,34 +4,23 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
 	output: "export",
 	trailingSlash: true,
+	distDir: "xpr/web",
+	assetPrefix: isProd
+		? "/__xpr__/pub_engine/playlist-nextjs/web/"
+		: undefined,
 
-	// expresia static deployment
-	// TODO!
-	// basePath: isProd ? "/__xpr__/pub_engine/playlist-nextjs/web" : "",
-	// assetPrefix: isProd ? "/__xpr__/pub_engine/playlist-nextjs/web/" : "",
-	assetPrefix: "/__xpr__/pub_engine/playlist-nextjs/web/",
-	// disable image optimization for static export compatibility
+	basePath: isProd
+		? "/__xpr__/pub_engine/playlist-nextjs/web"
+		: undefined,
+
 	images: {
 		unoptimized: true,
 	},
 
-	// expresia static deployment: resolving json data paths
-	// async rewrites() {
-	// 	return isProd
-	// 		? [
-	// 			{
-	// 				source: "/_next/data/:path*",
-	// 				destination: "/__xpr__/pub_engine/playlist-nextjs/web/_next/data/:path*"
-	// 			},
-	// 		]
-	// 		: [];
-	// },
-	
-	// env variables
 	env: {
 		DOMAIN_URL: process.env.DOMAIN_URL,
 		PUBLIC_API_BASE: process.env.PUBLIC_API_BASE,
-	},
+	}
 };
 
 export default nextConfig;
